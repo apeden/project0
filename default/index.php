e9e3305a8540a5cc2bad19467f64785c55fe0191
apeden <apeden23@gmail.com>
2019-06-18 00:40:32 +0100

    further changes to convert to Pizza. htaccess in default was changed to allow rewrite of ?= etc. This was crucial. Still don't fully understand why.

--- default/index.php
+++ default/index.php
@@ -17,18 +17,18 @@ switch ($page)
         render('templates/footer');
         break;
 
-    case 'lectures':
+    case 'pizzas':
 		render('templates/header', array('title' => 'Pizzas'));
-		render('lectures');
+		render('pizzas');
         render('templates/footer');
         break;
 
-    case 'lecture':
+    case 'pizza':
 
         if (isset($_GET['n']))
         {
-            render('templates/header', array('title' => 'Lecture ' . $_GET['n']));
-            render('lecture', array('n' => $_GET['n']));
+            render('templates/header', array('title' => 'Pizza ' . $_GET['n']));
+            render('pizza', array('n' => $_GET['n']));
             render('templates/footer');
         }
         break;
